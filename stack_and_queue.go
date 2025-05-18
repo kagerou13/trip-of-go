b15dc2dcbf2155e150a8dc110310796825e6b6b8
Ilham Wahyudi <f.valhalla13@gmail.com>
2025-05-18 15:49:30 +0700

    first commit

---
+++ stack_and_queue.go
@@ -0,0 +1,34 @@
+package tripofgo
+
+type Stack struct {
+	element []int
+}
+
+func (s *Stack) Push(n int) {
+	s.element = append(s.element, n)
+}
+
+func (s *Stack) Pop() int {
+	//if stack is empty
+	if len(s.element) == 0 {
+		panic("Empty stack")
+	}
+
+	//get last element from slice
+	top := s.element[len(s.element)-1]
+	// delete last element and update current element
+	s.element = s.element[:len(s.element)-1]
+
+	return top
+}
+
+func (s *Stack) Peek() int {
+	// if stack is empty
+	if len(s.element) == 0 {
+		panic("Empty stack")
+	}
+
+	top := s.element[len(s.element)-1]
+
+	return top
+}
