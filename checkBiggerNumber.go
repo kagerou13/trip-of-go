a26d4de054614fa33da679c6b92bc6b13ce8a6f9
Ilham Wahyudi <f.valhalla13@gmail.com>
2025-05-18 16:07:10 +0700

    second commit

--- checkBiggerNumber.go
+++ checkBiggerNumber.go
@@ -1,16 +1,14 @@
 package tripofgo
 
-func CheckBiggerNumber(n ...int) int {
+func CheckBiggerNumber(n ...int) (max int) {
 	if len(n) > 3 {
 		panic("Not allowed more than 3 numbers")
 	}
 
-	max := 0
 	for _, v := range n {
-		max = v
 		if max > v {
 			max = v
 		}
 	}
-	return max
+	return
 }
