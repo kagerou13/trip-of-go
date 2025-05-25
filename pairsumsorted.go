b9ac3fea616f2146a7519596880633dab236f051
Ilham Wahyudi <f.valhalla13@gmail.com>
2025-05-25 14:35:39 +0700

    update code

--- pairsumsorted.go
+++ pairsumsorted.go
@@ -9,24 +9,39 @@ func PairSumSorted(nums []int, target int) [][]int {
 		pairs [][]int
 	)
 
-	for left < right {
-		// add left nums value with right nums value
-		sum = nums[left] + nums[right]
-
-		//if sum is same with target
-		if sum == target {
-			pairs = append(pairs, []int{left, right})
-			left++
-			right--
+	// if len from nums is same with 3, iterate all elements
+	if len(nums) == 3 {
+		for i, v := range nums {
+			for in, val := range nums {
+				// if index is same, skip add to result slice
+				if i == in {
+					continue
+				} else if v == val {
+					pairs = append(pairs, []int{i, in})
+				}
+			}
 		}
+	} else {
+		for left < right {
+			// add left nums value with right nums value
+			sum = nums[left] + nums[right]
 
-		// if sum is less than target
-		if sum < target {
-			left++
-		} else {
-			// if sum is more than target
-			right--
+			//if sum is same with target
+			if sum == target {
+				pairs = append(pairs, []int{left, right})
+				left++
+				right--
+			}
+
+			// if sum is less than target
+			if sum < target {
+				left++
+			} else {
+				// if sum is more than target
+				right--
+			}
 		}
 	}
+
 	return pairs
 }
